7aa3142eb7760e3e8afc17ebed8d97aed3305ec6
jakeoostra <jake@oostra.me>
2026-04-20 02:11:50 -0500

    Versioning files

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"../app/page.tsx -> ./ClientApp\":{\"id\":\"../app/page.tsx -> ./ClientApp\",\"files\":[\"static/chunks/_app-pages-browser_app_ClientApp_tsx.js\"]}}"
